93064a24ffe477e194022d468a66461d8e76fa67
jennyjipark <jennyjipark@gmail.com>
2023-02-15 15:38:51 +0900

    eee

--- main.py
+++ main.py
@@ -166,25 +166,25 @@ async def send_meta_data(meta: MetaSchema=Body(default=None)):
 # 이미지 서버로 보내기
 @app.post("/send_images/", tags=["send image"])
 # async def send_images(files: List[UploadFile] = File(...)):
-async def send_images(files: List[UploadFile]):
+async def send_images(files: List[UploadFile] = File(...)):
     print("파일들", files) 
     
     file_urls = []
 
     # 파일을 푼다.
-    for file in files:
-        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        saved_file_name = f"image_{current_time}"
-        print(saved_file_name)
+    # for file in files:
+    #     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    #     saved_file_name = f"image_{current_time}"
+    #     print(saved_file_name)
 
-        file_location = os.path.join(IMG_DIR, saved_file_name)
+    #     file_location = os.path.join(IMG_DIR, saved_file_name)
 
-        # 보낸 파일을 쓴다.
-        with open(file_location, "wb+") as file_object:
-            file_object.write(file.file.read())
-        file_urls.append(SERVER_IMG_DIR + saved_file_name)
+    #     # 보낸 파일을 쓴다.
+    #     with open(file_location, "wb+") as file_object:
+    #         file_object.write(file.file.read())
+    #     file_urls.append(SERVER_IMG_DIR + saved_file_name)
 
-    result = {"file_urls": "나와라"}
+    # result = {"file_urls": "나와라"}
     
     return {"file_urls": "나와라"}
 
