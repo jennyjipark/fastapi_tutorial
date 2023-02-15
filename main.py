3d334d8ccde9aa25f8a13efeb4dde527780067b2
jennyjipark <jennyjipark@gmail.com>
2023-02-15 13:12:08 +0900

    11

--- main.py
+++ main.py
@@ -164,29 +164,29 @@ async def send_meta_data(meta: MetaSchema=Body(default=None)):
     }
 
 # 이미지 서버로 보내기 > 토큰 있어야 가능
-@app.post("/send_image/", tags=["send image"])
-async def send_image(files: List[UploadFile] = File(...)):
+@app.post("/send_images/", tags=["send image"])
+async def send_images(files: List[UploadFile] = File(...)):
 # async def send_image(file: UploadFile = File(...)):
     print("원본", files) 
     
-    file_urls = []
+    # file_urls = []
+
+    # # 파일을 푼다.
+    # for file in files:
+    #     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    #     saved_file_name = f"image_{current_time}"
+    #     print(saved_file_name)
+
+    #     file_location = os.path.join(IMG_DIR, saved_file_name)
+
+    #     # 보낸 파일을 쓴다.
+    #     with open(file_location, "wb+") as file_object:
+    #         file_object.write(file.file.read())
+    #     file_urls.append(SERVER_IMG_DIR + saved_file_name)
+
+    # result = {"file_urls" : file_urls}
     
-    # 파일을 푼다.
-    for file in files:
-        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        saved_file_name = f"image_{current_time}"
-        print(saved_file_name)
-
-        file_location = os.path.join(IMG_DIR, saved_file_name)
-
-        # 보낸 파일을 쓴다.
-        with open(file_location, "wb+") as file_object:
-            file_object.write(file.file.read())
-        file_urls.append(SERVER_IMG_DIR + saved_file_name)
-
-    result = {"file_urls" : file_urls}
-    
-    return "file"
+    # return "file"
 
 # 파일 저장
 @app.get("/images/{file_name}", tags=["get images"])
